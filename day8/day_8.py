345d082a765ec0c63323f7b27a68329f14e6855e
winklermi76145 <winklermi76145@th-nuernberg.de>
2024-12-10 10:37:56 +0100

    Day 8 part one done, dont quite understand the task in part 2

--- day8/day_8.py
+++ day8/day_8.py
@@ -1,8 +1,25 @@
+import math
+
 from utilities import read_file
 
 def print_map(map):
     for line in map:
         print(line)
+
+def get_antinode(v1, v2):
+    # Translate the system to origin
+    nv1 = [0, 0]
+    nv2 = [v2[0] - v1[0], v2[1] - v1[1]]
+
+
+    # Rotate 180 Deg
+    radians_180 = 180 * math.pi / 180
+    rv2 = [round(nv2[0] * math.cos(radians_180) - nv2[1] * math.sin(radians_180)), round(nv2[0] * math.sin(radians_180) + nv2[1] * math.cos(radians_180))]
+
+    # Translate back to startpoint
+    rv2 = [rv2[0] + v1[0], rv2[1] + v1[1]]
+
+    return rv2
 
 # Read input data
 input = read_file("input.txt")
@@ -18,13 +35,28 @@ for i in range(len(map)):
         if sign_number != 0:
             antennas[sign_number].append([i, j])
 
+count = 0
+antenna_pairs = []
 for frequency in antennas:
     if frequency == []:
         continue
-    print(frequency)
     for i, position in enumerate(frequency):
-        if i == len(frequency):
-            break
+        for j in range(len(frequency)):
+            if position != frequency[j]:
+                count += 1
+                antenna_pairs.append(list([position, frequency[j]]))
 
+for pair in antenna_pairs:
+    antinode = get_antinode(pair[0], pair[1])
+    print(antinode)
+    if antinode[0] < 0 or antinode[1] < 0:
+        continue
+    elif antinode[0] >= len(map) or antinode[1] >= len(map[0]):
+        continue
+    print(len(map), len(map[0]))
+    print(antinode)
+    map[antinode[0]][antinode[1]] = "#"
 
+antinodes = sum(row.count("#") for row in map)
 print_map(map)
+print(f"Sum of antinodes: {antinodes}")
