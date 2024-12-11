180d652b1553efd95d728bffb4cb5461ca6ee5d2
winklermi76145 <winklermi76145@th-nuernberg.de>
2024-12-11 15:27:47 +0100

    End of lecture

--- day9/day_9.py
+++ day9/day_9.py
@@ -2,6 +2,12 @@ import math
 
 from utilities import read_file
 
+def del_last_if_point(arr):
+    del arr[-1]
+    if arr[-1] == ".":
+        arr = del_last_if_point(arr)
+    return arr
+
 # Read input data
 input = read_file("input.txt")
 
@@ -18,4 +24,29 @@ for i in range(len(line)):
         for j in range(int(line[i])):
             new_line += str(".")
 
-print(new_line)
+
+arr = []
+for e in new_line:
+    if e == ".":
+        arr.append(e)
+    else:
+        arr.append(int(e))
+
+print("Sort array...")
+
+
+for i, e in enumerate(arr):
+    if arr[-1] == ".":
+        arr = del_last_if_point(arr)
+    if e == ".":
+        arr[i] = arr[-1]
+        del arr[-1]
+
+
+print(f"Array sorted!\n\n{arr}\n\nCalculation sum...")
+sum = 0
+for i, e in enumerate(arr):
+    if e == ".":
+        continue
+    sum += e * i
+print("Calculated sum:", sum)
